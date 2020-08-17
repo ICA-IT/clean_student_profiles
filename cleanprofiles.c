f4e54efe4f232de69f6b6915d392a8fa0b1cb8c3
John Kuras <w7og@yahoo.com>
2020-08-16 18:37:08 -0700

    seconf commit

--- cleanprofiles.c
+++ cleanprofiles.c
@@ -5,12 +5,37 @@
  *      Author: dad
  */
 
-
+//#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main ()
 {
+	FILE *fp;
+	char * line = NULL;
+	size_t len = 250;
+	ssize_t read;
+	size_t size = 255;
+
+
+	line = (char*)malloc(size);
+
+	fp=fopen("StudentGroup.txt", "r");
+	if (fp == NULL)return(1);
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+	        printf("Retrieved line of length %zu:\n", read);
+	        //fwrite(line, read, 1, stdout);
+	        printf(" line found : \"%s\"\n", line);
+	    }
+
+
+
+
+
+	fclose(fp);
+	if (line)free(line);
 	return (0);
 }
 
