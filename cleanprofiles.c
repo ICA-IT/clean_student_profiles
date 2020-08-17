5746a1f7c1aedf09678f4b080b89ac067221edfd
John Kuras <w7og@yahoo.com>
2020-08-17 09:34:45 -0700

    add more comments to the c program

--- cleanprofiles.c
+++ cleanprofiles.c
@@ -8,6 +8,19 @@
  *		The program expects an 8-bit ascii input file. Windows may produce
  *		wide format (16 bit) characters
  *
+ *		To create the initial input file (StudentGroup.txt), run the following
+ *		command on a windows domain member in an elevated cmd window:
+ *		dsget group "CN=ICA students,OU=students,OU=ICA users,DC=campus,DC=islandchristianacademy,DC=com" -members > StudentGroup.txt
+ *
+ *		The first output file (GetSutdentSIDs.ps) converts the list of ICA STudent group members
+ *		to a list of SIDs. I run it as a powershell script back on your domain Windows computer.
+ *		(It should work in an elevated cmd window, too.)
+ *
+ *		Copy the SIDs file created by the GetSutdentSIDs.ps script back to wherever you are
+ *		running this program and re-run it. The second run will produce the final output file
+ *		(CleanStudentProfiles.ps). Run this powershell script on each domain computer that you
+ *		wish to clean.
+ *
  *      Note: don't forget to run
  *      set-executionpolicy remotesigned
  *      in powershell before attempting to run the output script!
@@ -19,6 +32,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+void trimleadingandTrailing(char *s);
 
 int main ()
 {
@@ -48,10 +62,49 @@ int main ()
 	    	fprintf(fp2, "dsquery * %s -scope base -attr objectSid %s studentSIDs.txt\n",line,redir);
 	    	redir = redir2;
 	    }
+	fclose(fp);
+	fclose(fp2);
+
+	fp=fopen("studentSIDs.txt", "r");
+	if (fp == NULL)return(3);
+
+	fp2=fopen("CleanStudentProfiles.ps", "w");
+	if (fp2 == NULL)return(4);
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+//	        printf("Retrieved line of length %zu:\n", read);
+			line[read-1] = '\000';
+			trimleadingandTrailing(line);
+	        printf(" line found : \"%s\"\n", line);
+	        if ( strcmp(line,"objectSid") != 0 ) return(5);
+	        if ( (read = getline(&line, &len, fp)) == -1) return (6);
+	        line[read-1] = '\000';
+	        trimleadingandTrailing(line);
+	        printf(" line found : \"%s\"\n", line);
+	    	fprintf(fp2, "Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.SID -eq \'%s\' }  | Remove-CimInstance\n",line);
+	    }
 
 	fclose(fp);
+	fclose(fp2);
 	if (line)free(line);
 	return (0);
 }
 
+void trimleadingandTrailing(char *s)
+{
+	int  i,j;
 
+	for(i=0;s[i]==' '||s[i]=='\t';i++);
+
+	for(j=0;s[i];i++)
+	{
+		s[j++]=s[i];
+	}
+	s[j]='\0';
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]!=' '&& s[i]!='\t')
+				j=i;
+	}
+	s[j+1]='\0';
+}
